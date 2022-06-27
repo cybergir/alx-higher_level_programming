0b38aab6f4ffecf2f00d8acdf9ff546ed6f2eee3
Ruby <dorothymwende505@gmail.com>
2022-06-28 00:02:04 +0300

    advanced task, task 11

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,8 @@
+
+#!/usr/bin/python3
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[[A[[B[C[C[C[C[C[C[C[C[C[C[C[C[#!/usr/bin/python3
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+exit(1)
