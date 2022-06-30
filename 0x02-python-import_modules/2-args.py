bf4d12399e84e0cd095e53220b7822636c333bab
Ruby <dorothymwende505@gmail.com>
2022-06-30 21:40:13 +0300

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    if len(argv) == 1:
-        print("{} arguments.".format(0))
-    elif len(argv) == 2:
-        print("{} argument:".format(len(argv)-1))
-        print("{}: {}".format(len(argv)-1, argv[len(argv)-1]))
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(len(argv)-1))
-        for i in range(len(argv)):
-            if i == 0:
-                continue
-            print("{}: {}".format(i, argv[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
