21f8c44235059d2c923c3f1ad376f71ab405c684
Ruby <dorothymwende505@gmail.com>
2022-07-02 20:43:43 +0300

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,96 +1,21 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * reverse - reverses a linked list
- * @head: pointer to pointer to the first node
- *
- * Return: head pointer, pointing to reversed list
- */
-listint_t *reverse(listint_t **head)
-{
-	listint_t *front;
-	listint_t *back;
-
-	back = *head;
-	*head = (*head)->next;
-	back->next = NULL;
-
-	front = *head;
-	*head = (*head)->next;
-
-	while (1)
-	{
-		if (*head == NULL)
-		{
-			front->next = back;
-			*head = front;
-			break;
-		}
-		front->next = back;
-		back = front;
-
-		front = *head;
-		*head = (*head)->next;
-	}
-	return (*head);
-}
-
-/**
- * is_paplindrome - checks if a singly linked list is palindrome
- * @head: pointer to pointer to list's first node
- *
- * Description: An empty list is considered a palindrome
- *
- * Return: 1 if it's a palindrome, or 0 otherwise
+ * is_palindrome - check if a linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 or 1
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp; /* traverse the list to get its length */
-	listint_t *front; /* upper half of the list */
-	listint_t *back; /* lower half */
+    const listint_t *current;
+    const listint_t *tail;
+    unsigned int n = 0;
 
-	listint_t *tmp_front; 		/* traverse the upper half of the list */
+    //Get length
+    //Use a function to get a node on a specific index
+    //check the first to the last and, second to second last ....
+    //return false if one check was False
+    //return true at the end (if all passed the checks)
 
-	int n;	/* length of the list */
-	int half = 0;
-	int palind = 1;
-
-	if (!(*head))
-		return (0);
-	
-	tmp = *head;
-	n = 0;
-	while (tmp != NULL)
-	{
-		tmp = tmp->next;
-		n++;
-	}
-
-	back = front = *head;
-
-	while (half < n/2)
-	{
-		front = front->next;
-		half++;
-	}
-
-	reverse(&front);	/* reverse the upper half of the list */
-	tmp_front = front;
-
-	while (tmp_front != NULL)
-	{
-		if (tmp_front->n != back->n)
-		{
-			palind = 0;
-			break;
-		}
-		tmp_front = tmp_front->next;
-		back = back->next;
-	}
-	reverse(&front);	/* reconstitute the original list */
-
-	if (palind == 0)
-		return (0);
-	return (1);
+    return (n);
 }
