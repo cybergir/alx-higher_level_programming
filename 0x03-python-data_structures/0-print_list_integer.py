323d682b7019a251303c930e9bff9dd3b21efb00
Ruby <dorothymwende505@gmail.com>
2022-07-02 20:02:09 +0300

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_list_interger[D[D[[C[[C[ger(my_list=[]);
-[D[D[Dfor i in my_list:[A[C[C[C[C[C[C[C[C[C[C[C[C[C[:[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D
-        print("{:d}".format(i));
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
