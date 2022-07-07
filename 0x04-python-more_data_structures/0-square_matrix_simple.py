965dc895853fb35c008b0a5345987f1d4bb23dc4
Ruby <dorothymwende505@gmail.com>
2022-07-07 03:19:13 +0300

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        return matrix
-    new_matrix = []
-    for line in matrix:
-        new_matrix.append([i**2 for i in line])
-    return new_matrix
+    return [list(map((lambda x: x * x), elm)) for elm in matrix]
