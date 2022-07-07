862fe125ed9a2329e4ed29b6798293c32f08be37
Ruby <dorothymwende505@gmail.com>
2022-07-07 03:21:12 +0300

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if not my_list:
+        return my_list
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
