b1ecdb35c83e7fa1e80212cbee7679a657864394
Ruby <dorothymwende505@gmail.com>
2022-07-20 05:12:27 +0300

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a Square class and initializes its size."""
+
+
+class Square:
+    """Square implementation."""
+    def __init__(self, size):
+        self.__size = size
