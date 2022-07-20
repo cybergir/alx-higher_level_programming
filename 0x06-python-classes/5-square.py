3bcc2834c0480c68d8f29ed11977f7bce8f9707e
Ruby <dorothymwende505@gmail.com>
2022-07-20 05:15:06 +0300

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Defines a Square class
+It implements value/type validation for its attributes
+    using getters and setters
+"""
+
+
+class Square:
+    """
+    Square implementation
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print('#' * self.__size)
