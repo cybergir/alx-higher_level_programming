c5823e004832c49c915addf6523a65f9cd531249
Ruby <dorothymwende505@gmail.com>
2022-08-01 21:35:07 +0300

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module '5-base_geometry' contains an empty class
+"""
+
+
+class BaseGeometry:
+    """Class is empty.
+    """
+    def area(self):
+        """Public instance methods that raises an exception
+        """
+        raise Exception('area() is not implemented')
