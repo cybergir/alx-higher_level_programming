909f431a07c94da6e970c9c153cc4b8d12f59ca1
Ruby <dorothymwende505@gmail.com>
2022-08-03 05:39:20 +0300

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""
-a script that adds all arguments
-to a Python list, and then save
-them to a file
-"""
+"""add item"""
 
 
+import json
 import sys
+import os.path
 
-if __name__ == '__main__':
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        list_items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        list_items = []
-    list_items.extend(sys.argv[1:])
-    save_to_json_file(list_items, "add_item.json")
+filename = "add_item.json"
+if os.path.isfile(filename):
+    obj = load_from_json_file(filename)
+else:
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, filename)
