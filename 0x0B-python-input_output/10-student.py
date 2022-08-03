6ec44249b2f4079464a35cd2279da85057427c47
Ruby <dorothymwende505@gmail.com>
2022-08-03 05:26:48 +0300

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a single class: 'Student'"""
+
+
+class Student:
+    """Class Student."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a
+        Student instance.
+        """
+        if type(attrs) is list and all(isinstance(x, str) for x in attrs):
+            sub = {k: v for k, v in self.__dict__.items() if k in attrs}
+            return sub
+        return self._dict_
